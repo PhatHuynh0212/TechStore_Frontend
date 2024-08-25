5c528d6b5d7f19cd3be10f6352293d9eb7f058e3
huynhthanhphat <thanhphat02122003@gmail.com>
2024-08-26 04:23:38 +0700

    react route dom

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./redux/slides/counterSlice";
-import styled from "styled-components";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routes } from "./routes";
+import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
+import DefaultComponent from "./components/DefaultComponent/DefaultComponent";
 
 function App() {
-    const count = useSelector((state) => state.counter.value);
-    const dispatch = useDispatch();
-
-    const Button = styled.button`
-        background-color: orange;
-    `;
-
     return (
         <div>
-            <div>
-                <Button
-                    aria-label="Increment value"
-                    onClick={() => dispatch(increment())}
-                >
-                    Increment
-                </Button>
-                <span>{count}</span>
-                <Button
-                    aria-label="Decrement value"
-                    onClick={() => dispatch(decrement())}
-                >
-                    Decrement
-                </Button>
-            </div>
+            <Router>
+                <Routes>
+                    {routes.map((route, index) => {
+                        const Page = route.page;
+                        const LayoutHeader = route.isShowHeader
+                            ? DefaultComponent
+                            : Fragment;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <LayoutHeader>
+                                        <Page />
+                                    </LayoutHeader>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </Router>
         </div>
     );
 }
